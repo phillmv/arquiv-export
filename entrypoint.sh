0021f28fae66042bf2e55371643f1bbe79658d91
Phill MV <phillmv@okayfail.com>
2021-06-01 09:27:42 -0400

    wip, using arquivo to build the static site instead of middleman

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,23 +1,37 @@
 #!/bin/bash
 
 set -xeo pipefail
+cd /arquivo
+git pull
+echo "\n\n\n#########\nimporting\n-------------"
+STATIC_PLS=true NOTEBOOK_PATH=$GITHUB_WORKSPACE bundle exec rails static:import
+STATIC_PLS=true bundle exec rails s -d
+echo "\n\n\n#########\nbuilding\n-------------"
+STATIC_PLS=true bundle exec rails static:generate
+cd /arquivo/out
+echo "\n\n\n#########\npublishing\n-------------"
+if [ ! -z ${INPUT_CNAME} ]; then
+  echo ${INPUT_CNAME} > CNAME
+fi
 
-echo 'Installing bundles...'
+
+#
+# echo 'Installing bundles...'
 # cd ${INPUT_SITE_LOCATION}
 # gem install bundler
 # bundle install
 # bundle list | grep "middleman ("
-
-echo 'Building site...'
-cd /middleman
-bundle exec middleman build --verbose
-
-echo 'Publishing site...'
-cd /middleman/build
-if [ ! -z ${INPUT_CNAME} ]; then
-  echo ${INPUT_CNAME} > CNAME
-fi
-# cd ${INPUT_BUILD_LOCATION}
+#
+# echo 'Building site...'
+# cd /middleman
+# bundle exec middleman build --verbose
+#
+# echo 'Publishing site...'
+# cd /middleman/build
+# if [ ! -z ${INPUT_CNAME} ]; then
+#   echo ${INPUT_CNAME} > CNAME
+# fi
+# # cd ${INPUT_BUILD_LOCATION}
 remote_repo="https://${INPUT_GITHUB_ACTOR}:${INPUT_GITHUB_TOKEN}@github.com/${INPUT_GITHUB_REPOSITORY}.git" && \
 remote_branch=${INPUT_REMOTE_BRANCH}
 git init
@@ -26,11 +40,11 @@ git config user.email "${INPUT_GITHUB_ACTOR}@users.noreply.github.com"
 git add .
 echo -n 'Files to Commit:'
 ls -l | wc -l
-echo 'Committing files...'
-git commit -m'Middleman build' > /dev/null 2>&1
-echo "Pushing... to $remote_repo master:$remote_branch"
+# echo 'Committing files...'
+git commit -m'Arquivo build.' > /dev/null 2>&1
+# echo "Pushing... to $remote_repo master:$remote_branch"
 git push --force $remote_repo master:$remote_branch > /dev/null 2>&1
-echo "Removing git..."
+# echo "Removing git..."
 rm -fr .git
-cd -
+# cd -
 echo 'Done'
