728b90241f01bc120171c69c6d8ada13dc247bf6
Phill MV <phillmv@okayfail.com>
2021-06-20 21:14:14 -0400

    cleaned up action, entrypoint script

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,49 +2,37 @@
 
 set -xeo pipefail
 cd /arquivo
-git pull
 echo "\n\n\n#########\nimporting\n-------------"
 STATIC_PLS=true NOTEBOOK_PATH=$GITHUB_WORKSPACE bundle exec rails static:import
+
+# we boot up the server in the bg
 STATIC_PLS=true bundle exec rails s -d
+
 echo "\n\n\n#########\nbuilding\n-------------"
+
+# so we can fetch the static html
 STATIC_PLS=true bundle exec rails static:generate
 cd /arquivo/out
+
 echo "\n\n\n#########\npublishing\n-------------"
+
 if [ ! -z ${INPUT_CNAME} ]; then
   echo ${INPUT_CNAME} > CNAME
 fi
 
-
-#
-# echo 'Installing bundles...'
-# cd ${INPUT_SITE_LOCATION}
-# gem install bundler
-# bundle install
-# bundle list | grep "middleman ("
-#
-# echo 'Building site...'
-# cd /middleman
-# bundle exec middleman build --verbose
-#
-# echo 'Publishing site...'
-# cd /middleman/build
-# if [ ! -z ${INPUT_CNAME} ]; then
-#   echo ${INPUT_CNAME} > CNAME
-# fi
-# # cd ${INPUT_BUILD_LOCATION}
 remote_repo="https://${INPUT_GITHUB_ACTOR}:${INPUT_GITHUB_TOKEN}@github.com/${INPUT_GITHUB_REPOSITORY}.git" && \
 remote_branch=${INPUT_REMOTE_BRANCH}
+
 git init
 git config user.name "${INPUT_GITHUB_ACTOR}"
 git config user.email "${INPUT_GITHUB_ACTOR}@users.noreply.github.com"
 git add .
+
 echo -n 'Files to Commit:'
 ls -l | wc -l
-# echo 'Committing files...'
-git commit -m'Arquivo build.' > /dev/null 2>&1
-# echo "Pushing... to $remote_repo master:$remote_branch"
+
+git commit -m 'arquivo-export build.' > /dev/null 2>&1
 git push --force $remote_repo master:$remote_branch > /dev/null 2>&1
 # echo "Removing git..."
 rm -fr .git
-# cd -
 echo 'Done'
